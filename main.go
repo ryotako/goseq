43e3f103f3f653694e03e472a127936a15393acc
ryotako <ryotakov@gmail.com>
2018-02-02 21:16:28 +0900

    Rewrite main.go for testing

--- main.go
+++ main.go
@@ -3,28 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/dustin/go-humanize"
+	"io"
 	"os"
 	"strconv"
 )
 
-func usage() {
-	fmt.Println("usage: seq [first [incr]] last")
-	os.Exit(1)
+type CLI struct {
+	outStream, errStream io.Writer
 }
 
-func abend(s string) {
-	fmt.Fprintln(os.Stderr, "goseq:", s)
-	os.Exit(1)
+func main() {
+	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
+	os.Exit(cli.Run(os.Args[1:]))
+}
+
+func usage(o io.Writer) {
+	fmt.Fprintln(o, "usage: seq [first [incr]] last")
 }
 
 func printNum(n float64) {
 	fmt.Fprintln(os.Stdout, humanize.Ftoa(n))
 }
 
-func main() {
+func (c *CLI) Run(a []string) int {
 	fst, inc, lst := 1.0, 1.0, 1.0
-
-	a := os.Args[1:]
 
 	i, cnt := 0, 0
 	for i < len(a) {
@@ -32,7 +34,8 @@ func main() {
 		n, err := strconv.ParseFloat(a[i], 64)
 
 		if err != nil {
-			abend("invalid floating point argument " + a[i])
+			fmt.Fprintf(c.errStream, "goseq: invalid floating point argument %s\n", a[i])
+			return 1
 		}
 
 		switch cnt {
@@ -46,21 +49,25 @@ func main() {
 			inc, lst = lst, n
 			cnt++
 		default:
-			usage()
+			usage(c.outStream)
+			return 1
 		}
 
 		i++
 	}
 
 	if cnt == 0 {
-		usage()
+		usage(c.outStream)
+		return 1
 	}
 
 	if lst > fst {
 		if inc == 0 {
-			abend("zero increment")
+			fmt.Fprintln(c.errStream, "zero increment")
+			return 1
 		} else if inc < 0 {
-			abend("needs positive increment")
+			fmt.Fprintln(c.errStream, "needs positive increment")
+			return 1
 		} else {
 			for i := fst; i < lst+inc; i += inc {
 				printNum(i)
@@ -68,16 +75,18 @@ func main() {
 		}
 	} else {
 		if inc == 0 {
-			abend("zero increment")
+			fmt.Fprintln(c.errStream, "zero increment")
+			return 1
 		} else if fst == lst {
 			printNum(fst)
 		} else if inc > 0 {
-			abend("needs negative increment")
+			fmt.Fprintln(c.errStream, "needs negative increment")
+			return 1
 		} else {
 			for i := fst; i > lst+inc; i += inc {
 				printNum(i)
 			}
 		}
 	}
-
+	return 0
 }
